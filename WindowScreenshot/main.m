41ad32f7a3127264970e40632ee59fc458252952
Fernando Barajas <fernyb@fernyb.net>
2014-05-26 20:00:42 -0700

    Initial Commit

---
+++ WindowScreenshot/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  WindowScreenshot
+//
+//  Created by Fernando Barajas on 5/26/14.
+//  Copyright (c) 2014 Fernando Barajas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
